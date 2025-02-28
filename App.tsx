d0a04082730917443e92d112fd673b42739b14a2
muryllovieira <muryllovieira59@gmail.com>
2025-02-28 13:41:47 -0300

    adicionando o UseState

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import symbolOn from './assets/pictures/symbol-on.png';
 import symbolOff from './assets/pictures/symbol-off.png';
 
 export default function App() {
+  const [isActive, setisActive] = useState(false)
 
-  const isActive = true;
+  function handleSymbol(){
+    
+    setisActive((oldValue:boolean) =>{
+      return !oldValue
+    })
+
+    console.log(isActive)
+  }
 
   return (
     <View style={isActive ? styles.containerOn : styles.containerOff}>
-      <Image 
-        source={isActive ? symbolOn : symbolOff}
-      />
+      <TouchableOpacity onPress={handleSymbol} >
+          <Image 
+            source={isActive ? symbolOn : symbolOff}
+          />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -25,7 +35,7 @@ const styles = StyleSheet.create({
   },
   containerOff: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: 'grey',
     alignItems: 'center',
     justifyContent: 'center',
   }
